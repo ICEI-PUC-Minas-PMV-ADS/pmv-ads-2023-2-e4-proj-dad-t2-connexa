62df9a0a0d2b9e59ca115af6dbea32902f5f3ff4
Your Name <you@example.com>
2023-09-26 19:30:29 -0300

    [#41] Downgrade da versão dotnet dos projetos 8 > 7

--- Microservices/GatewayAPI/GatewayAPI/obj/Debug/net8.0/GatewayAPI.AssemblyInfo.cs
+++ Microservices/GatewayAPI/GatewayAPI/obj/Debug/net8.0/GatewayAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GatewayAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a754eaeb9893fe16bc032f1bbd8c2fc6b6ec5394")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GatewayAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GatewayAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

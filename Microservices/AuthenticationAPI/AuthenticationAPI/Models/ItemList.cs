0f06071e973bb7326b2600066c8bc491da99ad3c
Bruno <bruno.ruas2@gmail.com>
2023-09-27 23:35:37 -0400

    feat: EntityFramework Models no Microsserviço de Auth

---
+++ Microservices/AuthenticationAPI/AuthenticationAPI/Models/ItemList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AuthenticationAPI.Models;
+
+public partial class ItemList
+{
+    public int ItemId { get; set; }
+
+    public string ItemNome { get; set; } = null!;
+
+    public string? ItemDescricao { get; set; }
+
+    public int? ListaId { get; set; }
+
+    public bool ItemStatus { get; set; }
+
+    public virtual Lista? Lista { get; set; }
+}
---
+++ Microservices/AuthenticationAPI/AuthenticationAPI/Models/Lista.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AuthenticationAPI.Models;
+
+public partial class Lista
+{
+    public int ListaId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public bool ListaPublica { get; set; }
+
+    public bool ListaStatus { get; set; }
+
+    public string? ListaDescricao { get; set; }
+
+    public string? ListaTitulo { get; set; }
+
+    public virtual ICollection<Convite> Convites { get; set; } = new List<Convite>();
+
+    public virtual ICollection<ItemList> ItemLista { get; set; } = new List<ItemList>();
+
+    public virtual User? User { get; set; }
+
+    public virtual ICollection<UserLista> UserLista { get; set; } = new List<UserLista>();
+}

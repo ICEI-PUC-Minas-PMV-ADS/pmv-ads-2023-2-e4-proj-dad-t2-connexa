294b255759e82ee621efeac56a5fc4f3f22910ff
Marina Duque <103227016+Marinaduque459@users.noreply.github.com>
2023-09-29 21:30:55 -0300

    endpoint

--- Microservices/ListAPI/ListAPI/Program.cs
+++ Microservices/ListAPI/ListAPI/Program.cs
@@ -104,7 +104,7 @@ app.MapGet("/lists/{idList}", async ([FromServices] IServiceProvider provider, i
         return Results.NotFound();
     }
 });
-app.MapDelete("/lists/{idList}", async ([FromServices] IServiceProvider provider, int idList) =>
+app.MapDelete("/lists", async ([FromServices] IServiceProvider provider, int idList) =>
 {
     using (var scope = provider.CreateScope())
     {
@@ -117,16 +117,30 @@ app.MapDelete("/lists/{idList}", async ([FromServices] IServiceProvider provider
         return Results.NotFound();
     }
 });
+app.MapDelete("/member", async ([FromServices] IServiceProvider provider, int idMember) =>
+{
+    using (var scope = provider.CreateScope())
+    {
+
+        var listDataAccess = scope.ServiceProvider.GetService<IListDataAccess>();
+
+        if (listDataAccess != null)
+            return Results.Ok(await listDataAccess.DeleteMemberAsync(idMember));
+
+        return Results.NotFound();
+    }
+});
+
 
 //Feito
 //GetListsByParticipantOrOwnerAsync(int idUser); /lists/relateds/{idUser}
 //GetListsByOwnerAsync(int idOwner); /lists/owner/{idOwner}
 //GetListsByParticipantAsync(int idParticipant); /lists/participant/{idParticipant}
 //GetListByIdAsync(int idList); /lists/{idList}
-
-// Endpoints que ainda faltam...
 //DeleteListAsync(int idList); /lists
 //DeleteMemberAsync(int idMember); /members
+
+// Endpoints que ainda faltam...
 //SaveMemberAsync(MemberListDTO listMember); /members
 //GetMembersByListAsync(int idList); /members
 

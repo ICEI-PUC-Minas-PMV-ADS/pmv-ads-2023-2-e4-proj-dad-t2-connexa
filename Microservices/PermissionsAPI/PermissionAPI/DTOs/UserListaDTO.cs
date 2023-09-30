b5185d292351631be405e6d950798e698f0ad4a7
Gabriel Oliveira <1396143@sga.pucminas.br>
2023-09-30 17:57:32 -0300

    feat: adding methods to permissionAPI

---
+++ Microservices/PermissionsAPI/PermissionAPI/DTOs/UserListaDTO.cs
@@ -0,0 +1,21 @@
+﻿using PermissionAPI.Models;
+
+namespace PermissionAPI.DTOs
+{
+    public class UserListaDTO
+    {
+        public int UserListaId { get; set; }
+
+        public int? ListaId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public bool UserListaStatus { get; set; }
+
+        public int? UserListaRole { get; set; }
+
+        public virtual Lista? Lista { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
--- Microservices/PermissionsAPI/PermissionAPI/Program.cs
+++ Microservices/PermissionsAPI/PermissionAPI/Program.cs
@@ -1,4 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using PermissionAPI.DataAccess;
+using PermissionAPI.DTOs;
+using PermissionAPI.Interfaces;
+using PermissionAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const string CONNEXA_ORIGIN = "connexaOrigin";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CONNEXA_ORIGIN,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:19006")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod(); // add the allowed origins  
+                      });
+});
+builder.Services.AddDbContext<ConnexaContext>();
+builder.Services.AddScoped<IPermissionDataAccess, PermissionDataAccess>();
+
 var app = builder.Build();
 
 
@@ -7,5 +29,62 @@ app.UsePathBase("/connexa/permission/api");
 app.MapGet("/",() => "Connexa Permission API is running :)");
 app.MapGet("test",() => "Connexa Permission API is running now, no problems...");
 
+app.MapPost("/permission", async ([FromServices] IServiceProvider provider, [FromBody] UserListaDTO user_lista) =>
+{
+    using (var scope = provider.CreateScope())
+    {
+        var listDataAccess = scope.ServiceProvider.GetService<IPermissionDataAccess>();
+
+        if (listDataAccess != null)
+            return Results.Ok(await listDataAccess.CreatePermissionList(user_lista.UserId, user_lista.ListaId, user_lista.UserListaRole));
+
+        return Results.NotFound();
+
+    }
+
+});
+
+app.MapPatch("/permission", async ([FromServices] IServiceProvider provider, [FromBody] UserListaDTO user_lista) =>
+{
+    using (var scope = provider.CreateScope())
+    {
+        var listDataAccess = scope.ServiceProvider.GetService<IPermissionDataAccess>();
+
+        if (listDataAccess != null)
+            return Results.Ok(await listDataAccess.ModifyUserPermissionList(user_lista.UserId, user_lista.ListaId, user_lista.UserListaStatus, user_lista.UserListaRole));
+
+        return Results.NotFound();
+
+    }
+
+});
+
+app.MapDelete("/permission/listas/{lista_id}/users/{user_id}", async ([FromServices] IServiceProvider provider, int lista_id, int user_id) =>
+{
+    using (var scope = provider.CreateScope())
+    {
+        var listDataAccess = scope.ServiceProvider.GetService<IPermissionDataAccess>();
+
+        if (listDataAccess != null)
+            return Results.Ok(await listDataAccess.RevokeUserPermissionList(user_id, lista_id));
+
+        return Results.NotFound();
+    }
+
+});
+
+app.MapPatch("/permission/listas/{lista_id}", async ([FromServices] IServiceProvider provider, int lista_id, [FromQuery] bool status_public) =>
+{
+    using (var scope = provider.CreateScope())
+    {
+        var listDataAccess = scope.ServiceProvider.GetService<IPermissionDataAccess>();
+
+        if (listDataAccess != null)
+            return Results.Ok(await listDataAccess.AlterListAccess(lista_id, status_public));
+
+        return Results.NotFound();
+    }
+
+});
 
 app.Run();

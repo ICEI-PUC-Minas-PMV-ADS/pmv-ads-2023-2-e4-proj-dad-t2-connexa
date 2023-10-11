c572f934e45cc9be51deabf59a23b0e0babada9c
Mikaelly Carneiro <mikaellyscarneiro@gmail.com>
2023-10-11 17:03:31 -0300

    feat: Implementa estrutura da tela de login

--- src/front-web/src/App.js
+++ src/front-web/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import logo from "./img/logo.png";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleLogin = () => {
+    setIsClicked(true);
+    // Implementar a lógica de autenticação aqui
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 500);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="login-container">
+      <h2>Login</h2>
+        <img src={logo} alt="Logo" className="logo" />
+        <input
+          type="email"
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={handleLogin}
+          className={`login-button ${isClicked ? "clicked" : ""}`}
         >
-          Learn React
-        </a>
-      </header>
+          Entrar
+        </button>
+      </div>
     </div>
   );
 }

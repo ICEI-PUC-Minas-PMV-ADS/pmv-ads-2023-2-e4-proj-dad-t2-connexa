226c346ad519f48bed06e3d69fd27a83332baa30
LucasCNS <celionuvem@gmail.com>
2023-10-28 09:54:39 -0300

    dev: primeiras alterações pg Home.

--- src/front-web/src/pages/Home/index.js
+++ src/front-web/src/pages/Home/index.js
@@ -5,8 +5,9 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Avatar from '@mui/material/Avatar';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp'; // Importe o ícone de saída
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import logo from "../../img/logo.png";
+import ListaComponent from './style';
 
 function Home({ handleLogout }) {
     const navigate = useNavigate();
@@ -18,16 +19,32 @@ function Home({ handleLogout }) {
     };
 
     return (
+        <div>
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" noWrap sx={{ flex: 1 }}>
                     <Avatar src={logo} alt="Logo" sx={{ width: 40, height: 40, mr: 2 }} />
                 </Typography>
+                <Link to="/listas-contribuo" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
+                    Listas que Contribuo
+                </Link>
+                <Link to="/criar-lista" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
+                    Criar Lista
+                </Link>
                 <IconButton color="inherit" onClick={handleLogoutClick}>
                     <ExitToAppIcon />
                 </IconButton>
             </Toolbar>
         </AppBar>
+
+        <div>
+            <Typography variant="h4" component="h2" gutterBottom>
+                Minhas Listas
+            </Typography>
+
+            <ListaComponent idOwner={1} />
+        </div>
+    </div>
     );
 }
 

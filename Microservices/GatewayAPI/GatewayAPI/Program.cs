26a1d18cb9adaafd4cc5a573e50a0f23e84f0180
Bruno <bruno.ruas2@gmail.com>
2023-10-28 20:53:13 -0400

    fix: ajusta frontend para consumir gateway

--- Microservices/GatewayAPI/GatewayAPI/Program.cs
+++ Microservices/GatewayAPI/GatewayAPI/Program.cs
@@ -8,8 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json",optional: false,reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
+const string CONNEXA_ORIGIN = "connexaOrigin";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CONNEXA_ORIGIN,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:19006/", "http://localhost:3000")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
 
+app.UseCors(CONNEXA_ORIGIN);
 app.MapGet("/",() => "Hello World!");
 app.MapGet("/redis", () =>
 {

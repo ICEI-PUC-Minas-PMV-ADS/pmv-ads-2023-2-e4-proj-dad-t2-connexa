dc78ba0cc5d7f31e59e712980b0e67e65728ad31
Diego Soares <dieguinsharp@gmail.com>
2023-10-29 17:52:57 -0300

    merge adjusts

--- src/front-web/src/App.js
+++ src/front-web/src/App.js
@@ -4,8 +4,8 @@ import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import Recovery from "./pages/Recovery";
 import ItemList from "./pages/ItemList/ItemList.tsx"
-import Home from "./pages/Home";
 import ItemListEdit from "./pages/ItemListEdit";
+import Home from "./pages/Home";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
@@ -84,7 +84,7 @@ function App() {
               <>
                 <Route path="/" element={<Home handleLogout={handleLogout} />} />
                 <Route path="list/:idList/itemListEdit" element={<ItemListEdit handleLogout={handleLogout} />} />
-                <Route path="/itemList" element={<ItemList handleLogout={handleLogout} idList={1} />} />
+                <Route path="list/:idList/itemlist" element={<ItemList handleLogout={handleLogout} />} />
               </>
             }
           </Routes>

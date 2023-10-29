e6abd2ccb651d9da6e78c45176f4116c6bde6dbc
Gabriel Oliveira <1396143@sga.pucminas.br>
2023-10-29 16:28:34 -0300

    ajustando para fazer push para main

--- src/front-web/src/pages/Home/index.js
+++ src/front-web/src/pages/Home/index.js
@@ -1,78 +1,65 @@
-import React, { useState, useEffect } from "react";
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { useNavigate } from 'react-router-dom';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'; // Importe o ícone de saída
+import { useNavigate, Link } from 'react-router-dom';
 import logo from "../../img/logo.png";
-import Popup from "reactjs-popup";
-import ListsService from "../../services/lists/ListsService"
-import NewUserListDTO from "../../services/lists/dtos/NewUserListaDTO";
+import ListaItens from './listcomponent';
+import { Button } from '@mui/material';
 
 function Home({ handleLogout }) {
     const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
     const handleLogoutClick = () => {
         localStorage.removeItem('isLogged');
-        handleLogout(false);
+        handleLogout(false); // Chama a função handleLogout para atualizar o estado
         navigate('/');
     };
 
-    const handleClick = () => {
-        setIsPopupOpen(true);
-    };
-
-    const handleSubmit = async () => {
-        const listsService = new ListsService();
-        const newUserListDTO = new NewUserListDTO(1, 34);
-        const response = await listsService.addParticipant(newUserListDTO);
-
-        if (response) {
-            setEmail("");
-            alert("Participante adicionado a lista");
-        } else {
-            alert("Participante não encontrado");
-        }
+    const openListItemPage = () => {
+        navigate('/itemList');
     };
 
     return (
         <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" noWrap sx={{ flex: 1 }}>
-                        <Avatar src={logo} alt="Logo" sx={{ width: 40, height: 40, mr: 2 }} />
-                    </Typography>
-                    <IconButton color="inherit" onClick={handleLogoutClick}>
-                        <ExitToAppIcon />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-            <div>
-                <Button onClick={handleClick}>Adicionar participante</Button>
-                <Popup
-                    class="my-popup"
-                    open={isPopupOpen}
-                    onClose={() => setIsPopupOpen(false)}
-                    width={400}
-                    height={200}
-                >
-                    <h2>Adicionar participante</h2>
-                    <input
-                        type="email"
-                        placeholder="E-mail"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <button onClick={handleSubmit}>Enviar</button>
-                    <button onClick={() => setIsPopupOpen(false)}>Fechar</button>
-                </Popup>
-            </div>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" noWrap sx={{ flex: 1 }}>
+                    <Avatar src={logo} alt="Logo" sx={{ width: 40, height: 40, mr: 2 }} />
+                </Typography>
+                <Link to="/listas-contribuo" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
+                    Listas que Contribuo
+                </Link>
+                <Link to="/criar-lista" style={{ textDecoration: 'none', color: 'inherit', marginRight: '20px' }}>
+                    Criar Lista
+                </Link>
+                <IconButton color="inherit" onClick={handleLogoutClick}>
+                    <ExitToAppIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+
+        <div>
+            <Typography variant="h4" component="h2" gutterBottom>
+                Minhas Listas
+            </Typography>
+
+            <ListaItens/>
+            <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 2 }}
+                    onClick={openListItemPage}>
+                    open list
+            </Button>
         </div>
+    </div>
     );
+        
 }
+
 export default Home;

d3a64fd063a799a081e6526ad051227c0f36e72f
Mikaelly Carneiro <mikaellyscarneiro@gmail.com>
2023-11-15 18:23:09 -0300

    feat: implementa Bottom Tabs.

--- src/front-app/App.tsx
+++ src/front-app/App.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,13 +6,18 @@ import { PaperProvider } from 'react-native-paper';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
 import Recovery from './pages/Recovery';
+import Home from './pages/Home';
+import ExamplePage1 from './pages/ExamplePage1';
+import ExamplePage2 from './pages/ExamplePage2';
+import ExamplePage3 from './pages/ExamplePage3';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 export default function App() {
 
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   return (
     <PaperProvider>
@@ -29,6 +33,52 @@ export default function App() {
 const AuthenticatedContainer = () => {
   return (
     <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Página Inicial',
+            tabBarLabel: 'Início',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ExamplePage1"
+          component={ExamplePage1}
+          options={{
+            title: 'Página de Exemplo 1',
+            tabBarLabel: 'Exemplo 1',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ExamplePage2"
+          component={ExamplePage2}
+          options={{
+            title: 'Página de Exemplo 2',
+            tabBarLabel: 'Exemplo 2',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ExamplePage3"
+          component={ExamplePage3}
+          options={{
+            title: 'Página de Exemplo 3',
+            tabBarLabel: 'Exemplo 3',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
@@ -44,12 +94,3 @@ const UnauthenticatedContainer = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/front-app/pages/Home/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        marginTop: 60,
+        marginBottom: 30
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: '#134074',
+    }
+});
+
+export default styles;
